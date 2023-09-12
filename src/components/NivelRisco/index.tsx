7458e8ca76be1cc745bcee48a5230ce0c5d2f3f4
inovacao <inovacaobarueri@gmail.com>
2023-09-12 11:40:23 -0300

    Startando o projeto

---
+++ src/components/NivelRisco/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './main.css'
+
+export const NivelRisco = () => {
+    return(
+        <>
+            <div className='nivelRisco'>
+                <p className='textoNivelRisco'>Risco médio</p>
+            </div>
+        </>
+    )
+}
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,23 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import logo from './../../src/images/logo.png'
+import Card from '../components/Card';
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
+        
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Alertas</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <Card/>
+        {/* <ExploreContainer name="Tab 1 page" /> */}
       </IonContent>
     </IonPage>
   );
